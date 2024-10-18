78373ec67a856b7cceadcf5f11fa9d01e3404253
Josh Evans <82676600+Numberfiend@users.noreply.github.com>
2024-10-18 01:16:38 +0100

    Behaviour Tree Assets added

---
+++ Fantasy Quest/Assets/Behaviour_Tree_Assets/BehaviourTreeEditor.cs
@@ -0,0 +1,136 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.Callbacks;
+using System;
+
+public class BehaviourTreeEditor : EditorWindow
+{
+    BehaviourTreeView treeView;
+    InspectorView inspectorView;
+    IMGUIContainer blackboardView;
+
+    SerializedObject treeObject;
+    SerializedProperty blackboardProperty;
+    [MenuItem("BehaviourTreeEditor/Editor ...")]
+    public static void OpenWindow()
+    {
+        BehaviourTreeEditor wnd = GetWindow<BehaviourTreeEditor>();
+        wnd.titleContent = new GUIContent("BehaviourTreeEditor");
+    }
+
+    [OnOpenAsset]
+    public static bool OnOpenAsset(int instanceId, int line)
+    {
+        if(Selection.activeObject is BehaviourTree)
+        {
+            OpenWindow();
+            return true;
+        }
+        return false;
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+        // Instantiate UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Behaviour_Tree_Assets/BehaviourTreeEditor.uxml");
+        visualTree.CloneTree(root);
+
+
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Behaviour_Tree_Assets/BehaviourTreeEditor.uss");
+        
+        root.styleSheets.Add(styleSheet);
+
+        treeView = root.Q<BehaviourTreeView>();
+        inspectorView = root.Q<InspectorView>();
+        blackboardView = root.Q<IMGUIContainer>();
+        blackboardView.onGUIHandler = () =>
+        {
+            treeObject.Update();
+            EditorGUILayout.PropertyField(blackboardProperty);
+            treeObject.ApplyModifiedProperties();
+        };
+        treeView.OnNodeSelected = OnNodeSelectionChange;
+        OnSelectionChange();
+    }
+
+    private void OnEnable()
+    {
+        EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+
+    private void OnDisable()
+    {
+        EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+    }
+
+    private void OnPlayModeStateChanged(PlayModeStateChange obj)
+    {
+        switch (obj)
+        {
+            case PlayModeStateChange.EnteredEditMode:
+                OnSelectionChange();
+                break;
+            case PlayModeStateChange.ExitingEditMode:
+                break;
+            case PlayModeStateChange.EnteredPlayMode:
+                OnSelectionChange();
+                break;
+            case PlayModeStateChange.ExitingPlayMode:
+                break;
+        }
+    }
+
+    private void OnSelectionChange()
+    {
+        BehaviourTree tree = Selection.activeObject as BehaviourTree;
+        if (!tree)
+        {
+            if (Selection.activeGameObject)
+            {
+                BehaviourTreeRunner runner = Selection.activeGameObject.GetComponent<BehaviourTreeRunner>();
+                if (runner)
+                {
+                    tree = runner.tree;
+                }
+            }
+        }
+
+        if (Application.isPlaying)
+        {
+            if (tree)
+            {
+                treeView.PopulateView(tree);
+            }
+        }
+        else
+        {
+            if (tree && AssetDatabase.CanOpenAssetInEditor(tree.GetInstanceID()))
+            {
+                treeView.PopulateView(tree);
+            }
+        }
+
+
+        if (tree != null)
+        {
+            treeObject = new SerializedObject(tree);
+            blackboardProperty = treeObject.FindProperty("blackboard");
+        }
+        
+    }
+
+    void OnNodeSelectionChange(NodeView node)
+    {
+        inspectorView.UpdateSelection(node);
+    }
+
+    private void OnInspectorUpdate()
+    {
+        treeView?.UpdateNodeStates();
+    }
+}
