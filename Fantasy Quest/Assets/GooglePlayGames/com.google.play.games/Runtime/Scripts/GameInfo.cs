b9b268a5f916c10bd20aeaf9fa0f16efc400a467
Josh Evans <82676600+Numberfiend@users.noreply.github.com>
2025-01-07 01:06:59 +0000

    Google play login fixed

--- Fantasy Quest/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Fantasy Quest/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,10 +36,10 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "837873537020"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
-        public const string NearbyConnectionServiceId = "";
+        public const string WebClientId = "837873537020-klubq2g2stptf3ridh7ls3bs6ei65309.apps.googleusercontent.com"; // Filled in automatically
+        public const string NearbyConnectionServiceId = "com.DefaultCompany.FantasyQuest";
 
         public static bool ApplicationIdInitialized() {
             return !string.IsNullOrEmpty(ApplicationId) && !ApplicationId.Equals(ToEscapedToken(UnescapedApplicationId));
